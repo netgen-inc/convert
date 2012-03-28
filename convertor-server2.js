8a5e560c67158fcff3df667ffa300728fbd72d05
chenshijie <shijie.chen@gmail.com>
2012-03-28 23:31:35 +0800

    咨询类型的文章不发送给发送微博队列，改成发给主题提取队列

--- convertor-server2.js
+++ convertor-server2.js
@@ -14,6 +14,7 @@ var devent = require('devent').createDEvent(settings.devent.name);
 var queue = require('queuer');
 var deq = queue.getQueue(settings.queue.host,settings.queue.deqname);
 var enq = queue.getQueue(settings.queue.host,settings.queue.enqname);
+var enq4subject = queue.getQueue(settings.queue.host,settings.queue.enqname4subject);
 
 //事件通知有新任务
 devent.on('queued', function(msg){
@@ -47,8 +48,12 @@ var taskError = function(task) {
 };
 
 //微博发送通知事件
-var microPush=function(taskId){
-	enq.enqueue(settings.queue.enqurl+taskId);
+var microPush=function(taskId, micro_blog_type){
+	if(micro_blog_type == 'zixun') {
+		enq4subject.enqueue(settings.queue.enqurl4subject+taskId);
+	} else {
+		enq.enqueue(settings.queue.enqurl+taskId);
+	}
 };
 
 //从队列中加载任务
